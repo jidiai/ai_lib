8e3d219bf36b3dd8791bc687e820b9290e702110
zruizhi <ruizhi.zhang@ia.ac.cn>
2021-03-26 15:51:57 +0800

    update import

--- env/__init__.py
+++ env/__init__.py
@@ -3,5 +3,5 @@ from .reversi import *
 from .gobang import *
 from .sokoban import *
 from .ccgame import *
-# from .football import *
-# from .MiniWorld import *
+from .football import *
+from .MiniWorld import *
