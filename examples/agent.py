f2dc08767573c5930f391c0d486c99ae1ff43268
Yutongamber <yutongamber@yahoo.com>
2021-07-16 17:29:02 +0800

    update agent.py

--- examples/agent.py
+++ examples/agent.py
@@ -5,20 +5,17 @@ from algo.ddpg.ddpg import DDPG
 from algo.ac.ac import AC
 from algo.ddqn.ddqn import DDQN
 
+import os
 
 def ini_agents(args):
-    agent = None
-    if args.algo == "dqn":
-        agent = DQN(args)
-    elif args.algo == "pg":
-        agent = PG(args)
-    elif args.algo == "ddpg":
-        agent = DDPG(args)
-    elif args.algo == "ac":
-        agent = AC(args)
-    elif args.algo == "ppo":
-        agent = PPO(args)
-    elif args.algo == "ddqn":
-        agent = DDQN(args)
-    return agent
+
+    file_path = os.path.dirname(os.path.abspath(__file__)) + "/algo/" + args.algo + "/" + args.algo
+    print(file_path)
+    import_path = '.'.join(file_path.split('/')[-3:])
+    algo_name = args.algo.upper()
+    import_s = "from %s import %s as agent" % (import_path, algo_name)
+    print(import_s)
+    exec(import_s, globals())
+
+    return agent(args)
 
