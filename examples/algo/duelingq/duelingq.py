fcf0d4d97991b232b2ee86e561dfc5425743e209
Lc102197 <378845458@qq.com>
2021-07-19 13:44:48 +0800

    'message'

--- examples/algo/duelingq/duelingq.py
+++ examples/algo/duelingq/duelingq.py
@@ -5,6 +5,8 @@ import torch.nn.functional as F
 import torch.optim as optimizer
 import numpy as np
 
+from algo.duelingq.Network import Critic
+
 from pathlib import Path
 import sys
 base_dir = Path(__file__).resolve().parent.parent.parent
@@ -14,30 +16,6 @@ from common.buffer import Replay_buffer as buffer
 
 def get_trajectory_property():
     return ["action"]
-
-
-class Critic(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size):
-        super().__init__()
-        self.input_size = input_size
-        self.output_size = output_size
-        self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, 1)
-        self.linear3 = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        x1 = F.relu(self.linear1(x))
-        x2 = F.relu(self.linear1(x))
-
-        # value
-        y1 = self.linear2(x1)
-        # advantage
-        y2 = self.linear3(x2)
-        # y2.mean(dim=1,keepdim=True)：dim=1按行取均值；keepdim=True时，输出与输入维度相同，仅仅时输出在求均值的维度上元素个数变为1。keepdim=False时，输出比输入少一个维度，就是指定的dim求均值的维度。
-        x3 = y1 + y2 - y2.mean(dim=1, keepdim=True)
-
-        x = x3
-        return x
 
 
 # 定义Dueling DQN类（定义两个网络）
