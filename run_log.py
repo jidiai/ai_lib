66ccde2bc09db0aff2ef3f4d44136537c17c9f65
YanSong97 <yansong0902@gmail.com>
2021-10-18 17:30:42 +0800

    [olympics_running] update

--- run_log.py
+++ run_log.py
@@ -238,7 +238,7 @@ if __name__ == "__main__":
     # "magent-battle_v3-20v20", "gridworld", "cliffwalking", "smarts-loop", "sc2-MoveToBeacon", "olympics-running",
     # "smarts-cloverleaf", "smarts-figure_eight", "smarts-minicity", "smarts-straight", "smarts-zoo_intersection",
     # "smarts-ngsim-partial", "smarts-ngsim"
-    env_type = "smarts-ngsim-partial"
+    env_type = "olympics-running"
     game = make(env_type)
 
     # 针对"classic_"环境，使用gym core 进行render;
