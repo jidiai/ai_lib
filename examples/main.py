85f9aaa1848f8bcdd44a6556fbbc5729dacc1764
YanSong <yansong0902@gmail.com>
2023-01-03 04:47:58 +0800

    benchmark cartpole

--- examples/main.py
+++ examples/main.py
@@ -4,11 +4,11 @@ import argparse
 if __name__ == '__main__':
     # set env and algo
     parser = argparse.ArgumentParser()
-    parser.add_argument('--scenario', default="gridworld", type=str)
-    parser.add_argument('--algo', default="tabularq", type=str,
+    parser.add_argument('--scenario', default="classic_CartPole-v0", type=str)
+    parser.add_argument('--algo', default="sac", type=str,
                         help="tabularq/sarsa/dqn/ppo/ddpg/ac/ddqn/duelingq/sac/pg/sac/td3")
 
-    parser.add_argument('--reload_config', action='store_true')  # 加是true；不加为false
+    parser.add_argument('--reload_config', default='true')#, action='store_true')  # 加是true；不加为false
     args = parser.parse_args()
 
     print("================== args: ", args)
